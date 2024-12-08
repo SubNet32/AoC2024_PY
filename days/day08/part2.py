711e93b0e9783d752b2db9735a791c41e1bed08b
Philipp Priller <subnet32@gmail.com>
2024-12-08 09:33:40 +0100

    Day 8

---
+++ days/day08/part2.py
@@ -0,0 +1,52 @@
+import time
+from pathlib import Path 
+start_time = time.time()
+cwd = Path(__file__).parent
+path = cwd.joinpath("input.txt")
+file = open(path, "r").read().splitlines()
+
+antennas = {}
+maxY = len(file)-1
+maxX = len(file[0])-1
+for y in range(len(file)):
+    for x in range(len(file[y])):
+        c = file[y][x]
+        if(c == "."): continue
+        if(c not in antennas): antennas[c] = [(x,y)]
+        else: antennas[c].append((x,y))
+
+def subtractPoints(a:tuple, b:tuple):
+    return (a[0]-b[0], a[1]-b[1])
+
+def addPoints(a:tuple, b:tuple):
+    return (a[0]+b[0], a[1]+b[1])
+
+def checkInBounds(a:tuple):
+    return a[0] >= 0 and a[0] <= maxX and a[1] >= 0 and a[1] <= maxY
+
+def calcAntiNodeLine(a:tuple, dir:tuple):
+    result = [a]
+    pos = a
+    while(True):
+        pos = addPoints(pos, dir)
+        if(checkInBounds(pos)):
+            result.append(pos)
+        else:
+            break
+    return result
+
+def calcAntiNodesFor(a:tuple, b:tuple):
+    return calcAntiNodeLine(a, subtractPoints(a,b)) + calcAntiNodeLine(b, subtractPoints(b,a))
+    
+
+result = {}
+for a in antennas:
+    for i in range(len(antennas[a])-1):
+        for j in range(i+1, len(antennas[a])):
+            antiNodes = calcAntiNodesFor(antennas[a][i], antennas[a][j])
+            for p in antiNodes:
+                result[p] = True
+
+
+print("--- %s seconds ---" % (time.time() - start_time))
+print("result", len(result))
